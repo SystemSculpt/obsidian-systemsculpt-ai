31663764fd0b3bb3f2e43efed2b7d67ed3669cda
SystemSculpt <systemsculpt@gmail.com>
2024-09-30 13:47:56 +0800

    initial commit

---
+++ src/modules/recorder/settings/languageSetting.ts
@@ -0,0 +1,30 @@
+import { Setting } from 'obsidian';
+import { RecorderModule } from '../RecorderModule';
+
+const supportedLanguages: { [key: string]: string } = {
+  en: 'English',
+  es: 'Spanish',
+  fr: 'French',
+  de: 'German',
+  // Add more languages as needed
+};
+
+export function renderLanguageSetting(
+  containerEl: HTMLElement,
+  plugin: RecorderModule
+): void {
+  new Setting(containerEl)
+    .setName('Transcription Language')
+    .setDesc('Select the language of the audio for transcription')
+    .addDropdown(dropdown => {
+      for (const [code, language] of Object.entries(supportedLanguages)) {
+        dropdown.addOption(code, language);
+      }
+      dropdown
+        .setValue(plugin.settings.language)
+        .onChange(async (value: string) => {
+          plugin.settings.language = value;
+          await plugin.saveSettings();
+        });
+    });
+}
